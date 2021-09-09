3702fd68c3e1ba80944cbe41a191d6dd2507a0be
Percy-Potter2008 <75735895+Percy-Potter2008@users.noreply.github.com>
2021-09-09 19:47:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function setup() {
-  canvas = createCanvas(300, 400);
+  canvas = createCanvas(300, 300);
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
@@ -9,7 +9,7 @@ function modelLoaded() {
   console.log("model Loaded");
 }
 function draw() {
-  image(video, 0, 0, 300, 400);
+  image(video, 0, 0, 300, 300);
   classifier.classify(video, gotResults);
 }
 function gotResults(error, results) {
